4a0f66f33bde7aa2cecd26e11676669769186ca6
Илья Тетерин <45710701+iteterin@users.noreply.github.com>
2023-11-27 13:17:49 +0300

    fixec

--- FS/Get-ACLNTFS-Folder2.ps1
+++ FS/Get-ACLNTFS-Folder2.ps1
@@ -1,7 +1,7 @@
 $auditPath = "E:\FR2\"
 #$i = 0
 
-Get-ChildItem -Path $auditPath -Force | ?{ $_.PSIsContainer -and $_.FullName -match "maria.lazanskaya" -or $_.FullName -match "marina.r.volkova" -or $_.FullName -match "andrey.uvachev"} | %{
+Get-ChildItem -Path $auditPath -Force | ?{ $_.PSIsContainer -and $_.FullName -match "user.name" -or $_.FullName -match "user.name2" -or $_.FullName -match "user.name3"} | %{
     #if ($i -eq "5") {break}
     #else{
         $path = $_.Fullname
